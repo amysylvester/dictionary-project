ac96f1ed323ba3941019f4db0ed29d3317047d97
amysylvester <98198134+amysylvester@users.noreply.github.com>
2022-08-20 16:29:41 -0400

    Added axios and api

--- src/Search.js
+++ src/Search.js
@@ -1,12 +1,21 @@
 import React, { useState } from "react";
+import axios from "axios";
 import "./Search.css";
 
 export default function Search() {
   let [keyword, setKeyword] = useState("");
 
+  function handleResponse(response) {
+    console.log(response.data[0]);
+  }
+
   function search(event) {
     event.preventDevault();
-    alert(`Search definition of ${keyword}`);
+    alert(`Searching for ${keyword} definition`);
+
+    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
+    console.log(apiUrl);
+    axios.get(apiUrl).then(handleResponse);
   }
 
   function handleKeywordChange(event) {
