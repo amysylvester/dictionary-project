be0a3345c79d9f5604e1d6e21656a4314232da76
amysylvester <98198134+amysylvester@users.noreply.github.com>
2022-08-27 12:53:00 -0400

    Fixed link warning

--- src/Phonetic.js
+++ src/Phonetic.js
@@ -1,12 +1,11 @@
 import React from "react";
 
 export default function (props) {
-  console.log(props.phonetic);
   return (
     <div className="Phonetic">
       {props.phonetic.text}
       <br />
-      <a href={props.phonetic.audio} target="_blank">
+      <a href={props.phonetic.audio} target="_blank" rel="noopener noreferrer">
         Listen
       </a>
     </div>
